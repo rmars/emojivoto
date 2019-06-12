0a6a4a58da08f43293fbff209b340f654fe97c1e
Risha Mars <mars@buoyant.io>
2019-06-11 17:51:53 -0700

    Add Users to the GraphQL schema

--- graphql/schema.go
+++ graphql/schema.go
@@ -20,6 +20,11 @@ type (
 	helloArgs struct {
 		Name string
 	}
+
+	userResolver struct {
+		Resolver
+		u *db.User
+	}
 )
 
 const Schema = `
@@ -29,11 +34,36 @@ schema {
 
 type Query {
 	hello(name: String!): String!
+	users: [User]!
+}
+
+type User {
+	name: String!
+	favEmoji: String!
 }
 `
 
 func (r *Resolver) Hello(args helloArgs) string {
 	return fmt.Sprintf("Hello %s!", args.Name)
+}
+
+func (r *Resolver) Users() ([]*userResolver, error) {
+	users, err := r.db.GetUsers()
+
+	usersRsp := make([]*userResolver, 0)
+	for _, u := range users {
+		usersRsp = append(usersRsp, &userResolver{*r, u})
+	}
+
+	return usersRsp, err
+}
+
+func (r *userResolver) Name() string {
+	return r.u.Name
+}
+
+func (r *userResolver) FavEmoji() string {
+	return r.u.FavEmoji
 }
 
 func NewGraphQLServer(db *db.DBClient) *graphql.Schema {
